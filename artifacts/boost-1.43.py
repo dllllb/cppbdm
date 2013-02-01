a6743dea2095a99d6a7585e333e5bf55d34da476
Dmitri Babaev <dmitri.babaev@gmail.com>
2013-02-01 20:01:48 +0400

    boost mingw/msys build fixes

--- artifacts/boost-1.43.py
+++ artifacts/boost-1.43.py
@@ -1,22 +1,23 @@
 import subprocess as sp, os.path
 
 class Description:
-	def getArchiveType(self):
-		return 'tar.gz'
-	
-	def getDownloadUrls(self):
-		return ['http://sourceforge.net/projects/boost/files/boost/1.43.0/boost_1_43_0.tar.gz/download']
-		
-	def getDependencies(self):
-		return []
-		
-	def getIncludePaths(self):
-		return ['include']
-		
-	def getLibPaths(self):
-		return ['lib']
-		
-	def install(self, buildPath, targetPath, toolchain):
-		workDir = os.path.join(buildPath, 'boost_1_43_0')
-		sp.check_call(['bash', '-c', './bootstrap.sh --prefix=%s' % targetPath], cwd=workDir)
-		sp.check_call(['./bjam', 'install'], cwd=workDir)
+    def getArchiveType(self):
+        return 'tar.gz'
+    
+    def getDownloadUrls(self):
+        return ['http://sourceforge.net/projects/boost/files/boost/1.43.0/boost_1_43_0.tar.gz/download']
+        
+    def getDependencies(self):
+        return []
+        
+    def getIncludePaths(self):
+        return ['include']
+        
+    def getLibPaths(self):
+        return ['lib']
+        
+    def install(self, buildPath, targetPath, env):
+        workDir = os.path.join(buildPath, 'boost_1_43_0')
+        bootstrapCommand = ['./bootstrap.sh', '--prefix=%s' % targetPath]
+        sp.check_call(['bash', '-c', ' '.join(bootstrapCommand)], cwd=workDir)
+        sp.check_call(['./bjam', 'install'], cwd=workDir)
---
+++ artifacts/boost-1.52.py
@@ -0,0 +1,34 @@
+import subprocess as sp, os.path
+
+class Description:
+    def getArchiveType(self):
+        return 'tar.gz'
+    
+    def getDownloadUrls(self):
+        return ['http://sourceforge.net/projects/boost/files/boost/1.52.0/boost_1_52_0.tar.gz/download']
+        
+    def getDependencies(self):
+        return []
+        
+    def getIncludePaths(self):
+        return ['include']
+        
+    def getLibPaths(self):
+        return ['lib']
+        
+    def install(self, buildPath, targetPath, env):
+        workDir = os.path.join(buildPath, 'boost_1_52_0')
+        bootstrapCommand = ['./bootstrap.sh', '--prefix=%s' % targetPath]
+        if env.buildEnv == 'mingw':
+            bootstrapCommand.append('--with-toolset=mingw')
+        sp.check_call(['bash', '-c', ' '.join(bootstrapCommand)], cwd=workDir)
+        
+        #fix incorrectly generated jam file
+        if env.buildEnv == 'mingw':
+            with open('%s/project-config.jam' % workDir, "r") as conf:
+                contents = conf.read()
+                contents = contents.replace("mingw", "gcc")
+            with open('%s/project-config.jam' % workDir, "w") as conf:
+                conf.write(contents)
+        
+        sp.check_call(['%s/b2' % workDir, 'install'], cwd=workDir)
